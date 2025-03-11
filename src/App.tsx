56c75786cd18665b7fe4e28886576369a253c974
Chon Lou <163328241+ChonCanCode@users.noreply.github.com>
2025-03-11 17:06:43 +0000

    App.tsx/ fixing Router issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import "./index.css"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
 
 function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
