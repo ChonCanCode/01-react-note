abff9fe52e7f9cf123cb6c450f99cf89796d5434
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2025-03-12 08:46:42 +0000

    Understood issue with BrowerRouter needs to be added into main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css';
+import { Container } from "react-bootstrap";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { NewNote } from "./NewNote"
+import "./index.css";
 
 function App() {
   return (
-    <Router>
+  <Container>
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </Router>
+    </Container>
   );
 }
 
