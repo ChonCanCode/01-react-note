4e72d7a45b0d778330e3cf13f524b4b046d9316d
Chon Lou <163328241+ChonCanCode@users.noreply.github.com>
2025-03-17 14:58:35 +0000

    adding gap with bootstrap

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,3 +1,14 @@
+import { Form, Stack } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export function NoteForm () {
-    return <h1>I am here </h1>
+    return (
+    <Form>
+        <Stack gap={3}>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+        </Stack>
+    </Form>
+    )
 }
