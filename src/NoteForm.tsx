60bdfbfe19461855da7592137392f985b669add8
Chon Lou <163328241+ChonCanCode@users.noreply.github.com>
2025-03-18 17:08:50 +0000

    learning boosttrap Form Stack Row Col

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,13 +1,18 @@
-import { Form, Stack } from 'react-bootstrap';
+import { Form, Stack, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export function NoteForm () {
     return (
     <Form>
         <Stack gap={3}>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
+            <Row>
+                <Col>
+                    <Form.Group controlId='title'>
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control required />
+                    </Form.Group>
+                </Col>
+            </Row>
         </Stack>
     </Form>
     )
