5d032501b2ff564c55548ce752b2246e545a8ae9
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2025-03-23 08:36:27 +0000

    20250323 - 01 - update NoteForm

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,25 +1,38 @@
-import { Form, Stack, Row, Col } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, Stack, Row, Col, Button } from "react-bootstrap";
+import CreatableSelect from "react-select/creatable";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export function NoteForm () {
-    return (
+export function NoteForm() {
+  return (
     <Form>
-        <Stack gap={3}>
-            <Row>
-                <Col>
-                    <Form.Group controlId='title'>
-                        <Form.Label>Title</Form.Label>
-                        <Form.Control required />
-                    </Form.Group>
-                </Col>
-                <Col>
-                    <Form.Group controlId='tags'>
-                        <Form.Label>Tags</Form.Label>
-                        <Form.Control required />
-                    </Form.Group>
-                </Col>
-            </Row>
+      <Stack gap={3}>
+        <Row>
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control required />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <CreatableSelect isMulti />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Form.Group controlId="markdown">
+          <Form.Label>Body</Form.Label>
+          <Form.Control required as="textarea" rows={15} />
+        </Form.Group>
+        <Stack direction="horizontal" gap={2} className="justify-content-end">
+          <Button type="submit" variant="primary">
+            Save
+          </Button>
+          <Button type="button" variant="outline-secondary">
+            Cancel
+          </Button>
         </Stack>
+      </Stack>
     </Form>
-    )
+  );
 }
